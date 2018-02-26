9e503f2485dada63dabe558813de5431787b0ce7
David Sondak <dsondak@seas.harvard.edu>
2018-02-26 16:01:57 -0500

    Contour plots of E(k,y) and spectrum of E averaged over y.

--- post_process/Ek.py
+++ post_process/Ek.py
@@ -2,10 +2,12 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 import h5py
 import config    
 
 plt.rcParams.update(config.pars)
+np.set_printoptions(threshold=np.inf)
 
 def plot_energy_spectra():
 
@@ -16,14 +18,18 @@ def plot_energy_spectra():
    # Coordinates
    x = np.array(f['scales/x/1.0'])
    z = np.array(f['scales/z/1.0'])
+   dz = np.ediff1d(z)
+   Lz = 2.0
 
    Nx = x.shape[0]
    Nz = z.shape[0]
 
    # Solution fields
    T = np.array(f['tasks/b'])
+   Txy = T[1,:,:] # Get temperature field at specific time
    #T = T[10,:,Nz//2] # Get T near center and at specific time
    T = T[1,:,50] # Get T near the wall and at specific time
+
    #Ta = T - z[50]
 
    # Energy
@@ -45,6 +51,20 @@ def plot_energy_spectra():
    Tk = np.fft.fft(T) / Nx
    ETk = np.fft.fft(ET) / Nx
 
+   # Compute FFT of Txy in x
+   Tky = np.fft.fft(Txy, axis=0) / Nx
+   #print(Tky[:,50] - Tk)
+   
+   # Compute average of Txy in y
+   Tbar = np.zeros(Nx)
+   for ii in range(Nx):
+       for idy, deltaz in enumerate(dz):
+           Tbar[ii] += Txy[ii,idy+1] * deltaz
+   Tbar /= Lz
+
+   # Take FFT of Tbar
+   Tbark = np.fft.fft(Tbar) / Nx
+
    # Set up wavenumbers
    kx = np.zeros(Nx)
    for ii in range(Nx//2):
@@ -56,14 +76,18 @@ def plot_energy_spectra():
    NF = Nx
    kmax = NF // 2
    Ek = np.zeros(NF//2, dtype=np.complex_)
+   Ebark = np.zeros(NF//2, dtype=np.complex_)
    k  = np.arange(0.0, kmax)
    for ii in range(Nx):
       kmag = abs(kx[ii])
       if (kmag < kmax):
          Ek[int(kmag)] = Ek[int(kmag)] + 0.5*Tk[ii] * np.conj(Tk[ii])
+         Ebark[int(kmag)] = Ebark[int(kmag)] + \
+           0.5 * Tbark[ii] * np.conj(Tbark[ii])
 
    figE, axE = plt.subplots(1,1, figsize=(10,0.75*10))
    axE.plot(k, np.real(Ek))
+   axE.plot(k, np.real(Ebark))
    axE.set_xscale('log')
    axE.set_yscale('log')
    axE.set_xlabel(r'$k$')
@@ -94,6 +118,23 @@ def plot_energy_spectra():
 
    figA.tight_layout()
    figA.savefig('ET_ave_ywall.pdf')
+
+   # Create energy contours in Fourier space
+   NF = Nx
+   kmax = NF // 2
+   Eky = np.zeros([NF//2, Nz], dtype=np.complex_)
+   #Eky = np.zeros([Nz, NF//2], dtype=np.complex_)
+   K, Z = np.meshgrid(k, z)
+   for jj in range(Nz):
+       for ii in range(Nx):
+           kmag = abs(kx[ii])
+           if (kmag < kmax):
+              Eky[int(kmag), jj] = Eky[int(kmag), jj] + \
+                  0.5 * Tky[ii,jj] * np.conj(Tky[ii,jj])
+
+   figC, axC = plt.subplots(1,1, figsize=(10,0.75*10))
+   CS = axC.contourf(K, Z, np.real(np.transpose(Eky)), locator=ticker.LogLocator())
+   figC.colorbar(CS);
 
    plt.show()
 
